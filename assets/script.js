3b12524da749e4b9183d878cb86d6a6cdda6e16c
Kevin Lyons <zinckev@hotmail.com>
2020-12-21 14:16:06 -0800

    completed js api key check and added jquery to index.html along with html 5 basic set up

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+console.log("this is linked");
+
+var drinkIngrediant = "rum";
+var foodSearch = "chicken";
+
+var urlQueryDrinks =
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + drinkIngrediant;
+var urlQueryFood =
+  "https://www.themealdb.com/api/json/v1/1/search.php?s=" + foodSearch;
+
+$.ajax({
+  url: urlQueryFood,
+  method: "GET",
+}).then(function (foodSearchResults) {
+  console.log(foodSearchResults);
+});
+
+$.ajax({
+  url: urlQueryDrinks,
+  method: "GET",
+}).then(function (drinkSearchResults) {
+  console.log(drinkSearchResults);
+});
