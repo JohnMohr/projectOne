aa31c0a158e776dca57749e8ca62fb930401ceee
Kevin Lyons <zinckev@hotmail.com>
2020-12-31 12:39:21 -0800

    fixed html to included measurements for food and drink

--- assets/zestipesscript.js
+++ assets/zestipesscript.js
@@ -1,13 +1,1 @@
 console.log("this is linked");
-
-function getFood() {
-    var queryURLFood =
-      "https://www.themealdb.com/api/json/v1/1/search.php?s=" +
-      userFoodInput.val();
-    $.ajax({
-      url: queryURLFood,
-      method: "GET",
-    }).then(function (foodSearchResults) {
-
-        $("#dayOne").attr("src", foodSearchResults.meals[0].strMealThumb);
-    }
